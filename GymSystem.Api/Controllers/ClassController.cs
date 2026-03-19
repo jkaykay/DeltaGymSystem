14b8d67a97029d1c8841b047695c1dce106467ea
jkaykay <jayyang950@gmail.com>
2026-03-19 23:21:54 +0000

    Start Class and Session API

---
+++ GymSystem.Api/Controllers/ClassController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin,Staff,Trainer")]
+    public class ClassController : ControllerBase
+    {
+    }
+}
