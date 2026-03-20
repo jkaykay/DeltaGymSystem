a4796e61e0d006603d65151531e7bd1c37ab98de
jkaykay <jayyang950@gmail.com>
2026-03-20 17:16:52 +0000

    Trainer CRUD Ops endpoint setup

---
+++ GymSystem.Shared/DTOs/CreateTrainerRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace GymSystem.Shared.DTOs;
+
+public record CreateTrainerRequest(
+    string Email,
+    string FirstName,
+    string LastName,
+    string Password,
+    string EmployeeId
+    );
